a5cf6f7dc439801c05a63f6dc0afd9fdf58074a8
Muhammad Alam <malam53345@gmail.com>
2024-01-06 13:58:11 +0500

    detail page completed

---
+++ app/components/Rating.jsx
@@ -0,0 +1,55 @@
+import { AiOutlineStar } from "react-icons/ai";
+export default function Rating() {
+  return (
+    <div className="flex flex-col gap-[1rem]">
+      <h1 className="text-[20px] font-[600] text-white opacity-60 uppercase tracking-widest">
+        user ratings
+      </h1>
+      <div className="flex items-center gap-[20px]">
+        <div className="flex items-center gap-[5px]">
+          <AiOutlineStar className="text-[--pulper] text-[22px]" />
+          <h1 className="text-[18px] font-[700] text-white opacity-60">5</h1>
+        </div>
+        <div className="bg-[--primaryColor] w-full h-[0.7rem] rounded-full">
+          <div className="bg-[--pulper] w-[90%] h-full rounded-full"></div>
+        </div>
+      </div>
+      <div className="flex items-center gap-[20px]">
+        <div className="flex items-center gap-[5px]">
+          <AiOutlineStar className="text-[--pulper] text-[22px]" />
+          <h1 className="text-[18px] font-[700] text-white opacity-60">4</h1>
+        </div>
+        <div className="bg-[--primaryColor] w-full h-[0.7rem] rounded-full">
+          <div className="bg-[--pulper] w-[50%] h-full rounded-full"></div>
+        </div>
+      </div>
+      <div className="flex items-center gap-[20px]">
+        <div className="flex items-center gap-[5px]">
+          <AiOutlineStar className="text-[--pulper] text-[22px]" />
+          <h1 className="text-[18px] font-[700] text-white opacity-60">3</h1>
+        </div>
+        <div className="bg-[--primaryColor] w-full h-[0.7rem] rounded-full">
+          <div className="bg-[--pulper] w-[30%] h-full rounded-full"></div>
+        </div>
+      </div>
+      <div className="flex items-center gap-[20px]">
+        <div className="flex items-center gap-[5px]">
+          <AiOutlineStar className="text-[--pulper] text-[22px]" />
+          <h1 className="text-[18px] font-[700] text-white opacity-60">2</h1>
+        </div>
+        <div className="bg-[--primaryColor] w-full h-[0.7rem] rounded-full">
+          <div className="bg-[--pulper] w-[10%] h-full rounded-full"></div>
+        </div>
+      </div>
+      <div className="flex items-center gap-[20px]">
+        <div className="flex items-center gap-[5px]">
+          <AiOutlineStar className="text-[--pulper] text-[22px]" />
+          <h1 className="text-[18px] font-[700] text-white opacity-60">1</h1>
+        </div>
+        <div className="bg-[--primaryColor] w-full h-[0.7rem] rounded-full">
+          <div className="bg-[--pulper] w-[5%] h-full rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/Review.jsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { MdAccountCircle } from "react-icons/md";
+import { useState } from "react";
+export default function Review() {
+  const [message, setMessage] = useState(false);
+
+  const handleMessage = () => {
+    setMessage(true);
+  };
+
+  setTimeout(() => {
+    setMessage(false);
+  }, 3000);
+
+  return (
+    <div className="flex flex-col gap-[2rem]">
+      <h1 className="text-[20px] font-[600] text-white opacity-60 uppercase tracking-widest">
+        user reviews
+      </h1>
+      <div className="flex items-center gap-[1rem]">
+        <MdAccountCircle className="text-[--pulper] w-[50px] h-[50px]" />
+        <div className="flex flex-col gap-[5px]">
+          <h2 className="text-[16px] font-[600] text-[--pulper] tracking-widest uppercase">
+            sat, 28 mar 2023
+          </h2>
+          <p className="text-[16px] font-[600] text-white opacity-60">
+            Hi, I think its working very slow.
+          </p>
+        </div>
+      </div>
+      <div className="w-full h-[2px] bg-white opacity-20"></div>
+      <div className="flex items-center gap-[1rem]">
+        <MdAccountCircle className="text-[--pulper] w-[50px] h-[50px]" />
+        <div className="flex flex-col gap-[5px]">
+          <h2 className="text-[16px] font-[600] text-[--pulper] tracking-widest uppercase">
+            Fri, 07 Mar 2023
+          </h2>
+          <p className="text-[16px] font-[600] text-white opacity-60">
+            Worked as promised literally..
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-[1rem] mt-[2rem]">
+        <h1 className="text-[20px] font-[600] text-white opacity-60 uppercase tracking-widest">
+          leave a review
+        </h1>
+        <textarea
+          rows="7"
+          className="w-full rounded-[1rem] bg-[#30293a] outline-none text-white p-[1rem]"
+        ></textarea>
+        {message && (
+          <p className="text-[14px] font-[500] text-red-500">
+            You must download an application first to leave a review!
+          </p>
+        )}
+        <button
+          className="text-[14px] font-[700] text-white bg-[--pulper] py-[10px] px-[20px] rounded-[7px] uppercase w-full sm:w-fit"
+          onClick={handleMessage}
+        >
+          add review
+        </button>
+      </div>
+    </div>
+  );
+}
